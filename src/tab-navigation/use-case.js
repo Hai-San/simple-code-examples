bf98847560537bf83667f21f3af3e42edc12480f
Samuel <frontendsam@gmail.com>
2022-03-11 14:44:53 -0300

    Adds new tabs loading examples

--- src/tab-navigation/use-case.js
+++ src/tab-navigation/use-case.js
@@ -1,10 +1,23 @@
 const customTabNav = new TabNavigation();
 
 customTabNav.addEventListener('tabChange:start', (event) => {
-    if (event.data.newTab.id === 'tabExample_02') {
+    if (event.data.newTab.id === 'tabExample_02' && customTabNav.needLoading(event.data.newTab)) {
+        const tab = event.data.newTab;
+
+        console.dir(tab);
         setTimeout(() => {
-            customTabNav.hideLoading(event.data.newTab);
+            console.dir(tab);
+            customTabNav.hideLoading(tab);
         }, 10000);
+    }
+
+    if (event.data.newTab.id === 'tabExample_04' && customTabNav.needLoading(event.data.newTab)) {
+        const tab = event.data.newTab;
+
+        console.dir(tab);
+        setTimeout(() => {
+            customTabNav.hideLoading(tab);
+        }, 5000);
     }
 });
 
